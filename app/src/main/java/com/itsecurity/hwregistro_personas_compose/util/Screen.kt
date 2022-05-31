c7be85e3db337747c8cdb4c2512d96be848c63ee
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-05-31 14:07:19 -0400

    Aregando ultimo detalles al la route Screen

--- app/src/main/java/com/itsecurity/hwregistro_personas_compose/util/Screen.kt
+++ app/src/main/java/com/itsecurity/hwregistro_personas_compose/util/Screen.kt
@@ -1,12 +1,10 @@
 package com.itsecurity.hwregistro_personas_compose.util
 
-class Screen {
-    sealed class Screen(val route: String) {
-        object RegistroPersonaScreen: Screen("Registropersonas")
-        object ListadoPersonaScreen: Screen("Listadopersonas")
+ sealed class Screen(val route: String) {
+     object RegistroPersonaScreen: Screen("Registropersonas")
+     object ListadoPersonaScreen: Screen("Listadopersonas")
 
-        object RegistroOcupacionesScreen: Screen("RegistroOcupaciones")
-        object ListadoOcupacionesScreen: Screen("ListadoOcupacionesScreen")
+     object RegistroOcupacionesScreen: Screen("RegistroOcupaciones")
+     object ListadoOcupacionesScreen: Screen("ListadoOcupacionesScreen")
 
-    }
 }
