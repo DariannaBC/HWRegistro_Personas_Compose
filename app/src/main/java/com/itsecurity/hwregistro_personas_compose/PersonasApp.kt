67b020b5cb4add79cb83b40ee3b16d03e87d434c
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-06-04 22:14:44 -0400

    Aregando clase

---
+++ app/src/main/java/com/itsecurity/hwregistro_personas_compose/PersonasApp.kt
@@ -0,0 +1,8 @@
+package com.itsecurity.hwregistro_personas_compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PersonasApp : Application() {
+}
